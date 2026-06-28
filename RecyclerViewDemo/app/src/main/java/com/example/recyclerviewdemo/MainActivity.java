d4961a800c46f994785d1e2d6f3a0e03c1b14b82
AvishkarsGit <kumbharavishkar7@gmail.com>
2026-06-28 09:54:25 +0530

    crud operations added

--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.recyclerviewdemo;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
     private RecyclerView recyclerView;
+    private  ArrayList<CourseModel> courseList;
+    private CourseAdapter courseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
         //getAllList
         getAllList();
+
+        fab.setOnClickListener(v->{
+            openDialog();
+        });
     }
 
     private void getAllList() {
-        ArrayList<CourseModel> courseList = new ArrayList<>();
+        courseList = new ArrayList<>();
         // data 1
 //        CourseModel model = new CourseModel();
 //        model.setCourseTitle("C Language");
@@ -52,12 +61,63 @@ public class MainActivity extends AppCompatActivity {
 //        model2.setCoursePrice("399");
 //        courseList.add(model);
 
-        courseList.add(new CourseModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwYBiL19nAM-JCIlG0RY0NKpUmg6f0mQyA5ELUWmDEkg&s=10","C Lang","299"));
-        courseList.add(new CourseModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9ZrmsGwJYfYs_2ltjr0288zORq27l0AipBQwkbdtvMQ&s=10","C++ Lang","399"));
-        courseList.add(new CourseModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQClSrSy94fg7Y6VBv-HfVvCjzl17kfQTea2fOVE5oDAuSUA4wrpvxTEMY&s=10","Java Lang","499"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg","Python","599"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg","JavaScript","549"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg","TypeScript","649"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg","C#","699"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg","PHP","449"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg","Go","749"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-original.svg","Rust","899"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg","Kotlin","799"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg","Swift","849"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg","Dart","699"));
 
-        CourseAdapter courseAdapter =new CourseAdapter(MainActivity.this,courseList);
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/r/r-original.svg","R Programming","599"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg","Ruby","649"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/perl/perl-original.svg","Perl","499"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scala/scala-original.svg","Scala","799"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/elixir/elixir-original.svg","Elixir","849"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/haskell/haskell-original.svg","Haskell","899"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/lua/lua-original.svg","Lua","549"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matlab/matlab-original.svg","MATLAB","999"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/objectivec/objectivec-plain.svg","Objective-C","749"));
+        courseList.add(new CourseModel("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/clojure/clojure-original.svg","Clojure","799"));
+
+
+        courseAdapter =new CourseAdapter(MainActivity.this,courseList);
         recyclerView.setAdapter(courseAdapter);
+    }
+
+    private void openDialog(){
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.add_course_layout);
+
+        EditText edtCourseImage = dialog.findViewById(R.id.edtCourseImage);
+        EditText edtCourseTitle = dialog.findViewById(R.id.edtCourseTitle);
+        EditText edtCoursePrice = dialog.findViewById(R.id.edtCoursePrice);
+        Button btnAddAndEdit = dialog.findViewById(R.id.btnAddAndEdit);
+
+        btnAddAndEdit.setText("Save");
+
+        btnAddAndEdit.setOnClickListener(v ->  {
+            String image, title, price;
+            image = edtCourseImage.getText().toString();
+            title = edtCourseTitle.getText().toString();
+            price = edtCoursePrice.getText().toString();
+
+            CourseModel model = new CourseModel(image,title,price);
+            courseList.add(model);
+
+            courseAdapter.notifyItemInserted(courseList.size());
+            recyclerView.scrollToPosition(courseList.size()-1);
+
+
+            dialog.dismiss();
+
+        });
+
+        dialog.show();
+
     }
     private void initComp() {
         fab = findViewById(R.id.fabAdd);
--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/adapters/CourseAdapter.java
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/adapters/CourseAdapter.java
@@ -1,9 +1,14 @@
 package com.example.recyclerviewdemo.adapters;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -39,16 +44,56 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
         //bind data to your layout
-        String title = courseList.get(position).getCourseTitle();
-        String price = courseList.get(position).getCoursePrice();
-        String image = courseList.get(position).getCourseImage();
+        final CourseModel model = courseList.get(position);
+        String title = model.getCourseTitle();
+        String price = model.getCoursePrice();
+        String image = model.getCourseImage();
         holder.tvTitle.setText(title);
         holder.tvPrice.setText(price);
+
+
         try {
             Picasso.get().load(image).placeholder(R.drawable.ic_launcher_background).into(holder.courseIv);
         } catch (Exception e) {
             Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+        /*
+            item click for update
+         */
+        holder.itemView.setOnClickListener(v->{
+            openDialog(model, position);
+        });
+
+        // item long click for delete
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Delete Item");
+                builder.setMessage("Do you want to delete this item?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        courseList.remove(position);
+                        //notify adapter that we have removed item
+                        notifyItemRemoved(position);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
+                return true;
+            }
+        });
+
 
     }
 
@@ -56,5 +101,44 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseViewHolder> {
     public int getItemCount() {
         int size = courseList.size();
         return size;
+    }
+
+
+    private void openDialog(CourseModel model, int position) {
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.add_course_layout);
+
+        initDialogComp(dialog, model,position);
+
+        dialog.show();
+    }
+
+    private void initDialogComp(Dialog dialog, CourseModel model, int position) {
+        EditText edtCourseImage = dialog.findViewById(R.id.edtCourseImage);
+        EditText edtCourseTitle = dialog.findViewById(R.id.edtCourseTitle);
+        EditText edtCoursePrice = dialog.findViewById(R.id.edtCoursePrice);
+        Button btnAddAndEdit = dialog.findViewById(R.id.btnAddAndEdit);
+
+        //set data
+        edtCourseTitle.setText(model.getCourseTitle());
+        edtCoursePrice.setText(model.getCoursePrice());
+        edtCourseImage.setText(model.getCourseImage());
+        btnAddAndEdit.setText("Edit");
+
+        btnAddAndEdit.setOnClickListener(v->{
+            String image, title, price;
+            image = edtCourseImage.getText().toString();
+            title = edtCourseTitle.getText().toString();
+            price = edtCoursePrice.getText().toString();
+
+            CourseModel update = new CourseModel(image, title, price);
+            courseList.set(position,update);
+
+            notifyItemChanged(position);
+            // it will notify the adapter that where it has changed on which index
+
+            dialog.dismiss();
+        });
+
     }
 }
